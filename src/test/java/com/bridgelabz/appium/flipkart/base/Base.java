18f4367fc0642000c8fc5e9d82b7ac503ca4e9f4
unknown <pritampaul9797@gmail.com>
2021-11-17 12:23:14 +0530

    Implemented Takescreenshot interface

--- src/test/java/com/bridgelabz/appium/flipkart/base/Base.java
+++ src/test/java/com/bridgelabz/appium/flipkart/base/Base.java
@@ -1,12 +1,21 @@
 package com.bridgelabz.appium.flipkart.base;
 
 import io.appium.java_client.android.AndroidDriver;
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
+import java.io.File;
+import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class Base {
 
@@ -20,14 +29,24 @@ public class Base {
         cap.setCapability("udid", "emulator-5554");
         cap.setCapability("platformName", "Android");
         cap.setCapability("platformVersion", "8.0.0");
-        cap.setCapability("app","C:\\Users\\Pritam Paul\\" +
-                "Downloads\\demo1\\AmazonAutomation\\resources\\flipkart.apk");
+        cap.setCapability("app","C:\\Users\\Pritam Paul" +
+                "\\Downloads\\demo1\\AmazonAutomation\\resources\\apk\\flipkart.apk");
         cap.setCapability("appPackage","com.flipkart.android");
         cap.setCapability("autoGrantPermissions",true);
 
         Thread.sleep(6000);
         driver = new AndroidDriver(new URL("http://0.0.0.0:4723/wd/hub"), cap);
         Thread.sleep(6000);
+    }
 
+    @AfterTest
+    public void takeScreenshot() throws IOException {
+        DateFormat dateFormat = new SimpleDateFormat("dd-mm-yyyy h-m-s");
+        Date date = new Date();
+
+        File file = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+        FileUtils.copyFile(file, new File("C:\\Users\\Pritam Paul\\" +
+                "Downloads\\demo1\\AmazonAutomation\\resources\\screenshots" +
+                dateFormat.format(date) + "screenshot.png"));
     }
 }
